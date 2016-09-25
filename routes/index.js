dcaf45598d509d2c5f6a83629a7aa0450161dcff
Noah Rossi <me@noahrossi.com>
2016-09-25 18:33:59 +0000

    added more templating goodies

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var navItems = [{title:'Home', link:'/'},{title:'Something else', link:'/thing'}];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', section: 'Home', navItems: navItems});
+});
+
+router.get('/thing', function(req, res, next) {
+  res.render('index', { title: 'Thing', section: 'Something else', navItems: navItems});
 });
 
 module.exports = router;
