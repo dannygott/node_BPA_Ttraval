e4779d108ada6ff6b57b98224517433cfc9d5c2d
Noah Rossi <me@noahrossi.com>
2016-09-27 10:45:28 +0000

    destinations router

---
+++ routes/destinations.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    res.render('error', { message: 'No destination specified', error: { } });
+});
+
+router.get('/:id', function(req, res, next) {
+    res.render('index', { title: req.params.id }); 
+});
+
+module.exports = router;
