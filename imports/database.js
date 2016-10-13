b71bf2503cac445ece2d2e9d59d46feba3043e2b
Noah Rossi <me@noahrossi.com>
2016-10-13 16:27:30 +0000

    automatically create db/tables if they don't exist

--- imports/database.js
+++ imports/database.js
@@ -8,6 +8,22 @@ r.connect({
     if (err) throw err;
     conn = connection;
     console.log("DB Connected!");
+
+    r.dbCreate('travel').run(conn, function(err, result) {
+        if (err) {
+            console.log('Using existing travel DB');
+        }
+        else {
+            console.log('Creating travel DB...');
+        }
+    });
+    r.db('travel').tableCreate('destinations').run(conn, function(err, result) {
+        if (err) {
+            console.log('Using existing destinations table');
+        } else {
+            console.log('Creating destinations table...');
+        }
+    });
 });
 
 module.exports = {
