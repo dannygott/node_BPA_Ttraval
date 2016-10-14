c23e524512d3cb08fc015e42f5d8dab6731bc132
Noah Rossi <me@noahrossi.com>
2016-10-14 16:58:17 +0000

    pull dests into routes

--- routes/destinations.js
+++ routes/destinations.js
@@ -6,7 +6,10 @@ var db = require('../imports/database.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render('destination', {navItems: navItems, section: 'destinations' });
+    var dests = [{image: "cancun.jpeg", caption: "young bool betta come hea"}, {image: "cancun.jpeg", caption: "young bool betta come hea"},{image: "cancun.jpeg", caption: "young bool betta come hea"}, {image: "cancun.jpeg", caption: "young bool betta come hea"}, {image: "cancun.jpeg", caption: "young bool betta come hea"}, {image: "cancun.jpeg", caption: "young bool betta come hea"}, {image: "cancun.jpeg", caption: "young bool betta come hea"}];
+
+    res.render('destination', {navItems: navItems, section: 'destinations',
+        dests: dests });
 });
 
 router.get('/:id', function(req, res, next) {
