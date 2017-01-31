b6439d7ad15bfec108ce6212bd65c405ee0052b4
dannygott <dannygott98@gmail.com>
2017-01-31 18:05:26 -0500

    fixed the logic for form popping away

--- public/javascripts/loginfadr.js
+++ public/javascripts/loginfadr.js
@@ -4,9 +4,22 @@ function fadetologin(selector) {
       console.log("bool");
     });
   }else {
-    $(".login").fadeOut( 'slow', function(){
-      console.log("bool");
-    });
+    if (checkhover() != "INPUT" && checkhover() != "FORM") {
+      console.log(checkhover());
+      $(".login").fadeOut( 'slow', function(){
+        console.log("bool");
+      });
+    }
+
   }
 
 }
+function checkhover(){
+ var element = $(':hover');
+    if(element.length)
+    {
+        var domElement = element[element.length - 1];
+        var tagName = domElement.tagName;
+        return tagName
+    }
+  }
