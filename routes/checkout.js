0294e315a0a65532bf6d79dd57553890289e29b2
Noah Rossi <me@noahrossi.com>
2017-02-07 20:40:30 -0500

    get rid of default checkout page

--- routes/checkout.js
+++ routes/checkout.js
@@ -5,19 +5,6 @@ var navItems = require('../config.json').navItems;
 var db = require('../imports/database.js');
 var authorizeUser = require('../imports/auth.js').authorizeUser;
 
-/* GET sample checkout page. */
-router.get('/', function(req, res, next) {
-    db.getDests(function(err, cursor) {
-        cursor.toArray(function(err, dests) {
-            if (err) throw err;
-
-            res.render('checkout', {navItems: navItems, user: req.user,
-                section: 'checkout', dests: dests });
-        });
-
-    });
-});
-
 /* GET checkout page for a destination */
 router.get('/:id', authorizeUser(), function(req, res, next) {
     db.getDest(req.params.id, function(err, result) {
