ed533a6e8483e36d0cf0ecf46409b7a3b2baa7cd
dannygott <dannygott98@gmail.com>
2017-02-07 21:31:42 -0500

    SERCH BERR

---
+++ public/javascripts/currencyConvert.js
@@ -0,0 +1,13 @@
+var request = require('request');
+
+function convert(ammount, currencyFrom, currencyTo){
+  $.get('http://api.fixer.io/latest?base=USD', function( data ) {
+    JSON.parse(data);
+    if (currencyFrom == "USD") {
+      
+    }
+    return answer
+
+  });
+
+}
