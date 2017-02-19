0497c69f0f7d17b22e005a110ca74d112a6db27b
dannygott <dannygott98@gmail.com>
2017-02-18 22:43:19 -0500

    fixed Search. Added redirect to login on checkout without user

--- public/javascripts/hotelanimation.js
+++ public/javascripts/hotelanimation.js
@@ -1,11 +1,13 @@
-
-     $(".destbox").stop().fadeOut( 'slow', function(){
-     });
-     $( ".destButton" ).each(function( index ) {
-        var searchfor = $('.search').val().toUpperCase();
-        console.log(searchfor);
-       if ($(this).text().toUpperCase().includes(searchfor) == true){
-         $(this).parent().parent().parent().stop().fadeIn( 'fast', function(){
-         });
-       }
-     });
+function hotelinfo(dest, fade) {
+  if (fade == true) {
+    $(".hotelBox").stop().fadeOut( 'slow', function(){
+    });
+    $(".hotelInfo" + dest).stop().fadeIn( 'slow', function(){
+    });
+  }else {
+    $(".hotelBox").stop().fadeIn( 'slow', function(){
+    });
+    $(".hotelInfo" + dest).stop().fadeOut( 'slow', function(){
+    });
+  }
+}
