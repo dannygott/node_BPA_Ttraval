ed23c75b0d2a992949fa3007b01556f214a38053
dannygott <dannygott98@gmail.com>
2017-02-20 00:31:29 -0500

    removed logging from search

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,12 +1,10 @@
 var lastentry = "";
-
 $(".search").keyup(function(event) {
    if($('.search').val() != lastentry) {
      $(".destbox").stop().fadeOut( 'slow', function(){
      });
      $( ".destButton" ).each(function( index ) {
         var searchfor = $('.search').val().toUpperCase();
-        console.log(searchfor);
        if ($(this).text().toUpperCase().includes(searchfor) == true){
          $(this).parent().parent().parent().stop().fadeIn( 'fast', function(){
          });
