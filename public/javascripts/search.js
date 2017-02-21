8d84b3edea2bb5b230aebaaf568cfe5f9edf7778
dannygott <dannygott98@gmail.com>
2017-02-20 21:50:17 -0500

    Small Changes

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,10 +1,10 @@
-var lastentry = "";
-$(".search").keyup(function(event) {
-   if($('.search').val() != lastentry) {
-     $(".destbox").stop().fadeOut( 'slow', function(){});
-     $( ".destButton" ).each(function( index ) {
-        var searchfor = $('.search').val().toUpperCase();
-       if ($(this).text().toUpperCase().includes(searchfor) == true){
+var lastentry = ""; // var containing last entry in form
+$(".search").keyup(function(event) { // detect change in search form entry
+   if($('.search').val() != lastentry) { // filter old entries
+     $(".destbox").stop().fadeOut( 'slow', function(){}); // fade out all destinations
+     $( ".destButton" ).each(function( index ) { // filter all destinations on page for ones matching search
+        var searchfor = $('.search').val().toUpperCase();  // uppercase conversion
+       if ($(this).text().toUpperCase().includes(searchfor) == true){ // if the destination matches the searchterm fade its parents parents parent in
          $(this).parent().parent().parent().stop().fadeIn( 'fast', function(){
          });
        }
@@ -12,3 +12,4 @@ $(".search").keyup(function(event) {
    }
    lastentry = $('.search').val()
 });
+ // .stop is added to keep jquery stuff from queing
